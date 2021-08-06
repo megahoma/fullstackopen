fcdbee66ab4c3d4377e12bbf36ae36cd6186767f
megahoma <timkrut5348@gmail.com>
2021-08-07 00:18:26 +0300

    part_4 (Exercises 4.4)

--- part4/Bloglist/utils/list_helper.js
+++ part4/Bloglist/utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => {
   return 1
 }
+const totalLikes = (blogs) => {
+  let sum = 0
+  blogs.map((blog) => (sum += blog.likes))
+  return sum
+}
 
 module.exports = {
   dummy,
+  totalLikes,
 }
