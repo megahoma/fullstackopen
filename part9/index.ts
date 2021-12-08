08bbe19f0fbafa827133bf0b5551bb0004ba0a6e
megahoma <timkrut5348@gmail.com>
2021-12-08 10:21:45 +0300

    part9 (Exercises 9.6 - 9.7)

--- part9/index.ts
+++ part9/index.ts
@@ -1,15 +1,18 @@
-import express from 'express'
-const app = express()
+import express from 'express';
+const app = express();
 
-import calculateBmi from './bmiCalculator'
+import calculateBmi from './bmiCalculator';
+import calculateExercises from './trainingCalculator';
+
+app.use(express.json());
 
 app.get('/', (_req, res) => {
-  return res.send('Hello Full Stack!')
-})
+  return res.send('Hello Full Stack!');
+});
 
 app.get('/bmi', (req, res) => {
-  const weight = Number(req.query.weight)
-  const height = Number(req.query.height)
+  const weight = Number(req.query.weight);
+  const height = Number(req.query.height);
 
   if (!weight || !height) {
     return res
@@ -17,24 +20,50 @@ app.get('/bmi', (req, res) => {
       .json({
         error: 'malformatted parameters',
       })
-      .end()
+      .end();
   }
 
   try {
-    const bmi = calculateBmi(weight, height)
-    return res.status(200).json({ weight, height, bmi }).end()
+    const bmi = calculateBmi(weight, height);
+    return res.status(200).json({ weight, height, bmi }).end();
   } catch (error) {
     return res
       .status(400)
-      .type('application/json')
       .json({
         error: 'malformatted parameters',
       })
-      .end()
+      .end();
   }
-})
+});
 
-const PORT = 3003
+app.post('/calculator', (req, res) => {
+  const body = req.body;
+  const arr = body.daily_exercises;
+  const target = Number(body.target);
+
+  if (!arr || !target) {
+    return res
+      .status(400)
+      .json({
+        error: 'malformatted parameters',
+      })
+      .end();
+  }
+
+  try {
+    const result = calculateExercises(arr, target);
+    return res.status(200).send(result).end();
+  } catch (error) {
+    return res
+      .status(400)
+      .json({
+        error: 'malformatted parameters',
+      })
+      .end();
+  }
+});
+
+const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
--- part9/trainingCalculator.ts
+++ part9/trainingCalculator.ts
@@ -1,54 +1,53 @@
 interface ExercisesResult {
-  periodLength: number
-  trainingDays: number
-  success: boolean
-  rating: number
-  ratingDescription: string
-  target: number
-  average: number
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
 }
 
 interface ExercisesInputType {
-  arr: number[]
-  target: number
+  arr: number[];
+  target: number;
 }
 
 const parseArguments = (args: Array<string>): ExercisesInputType => {
-  if (args.length < 4) throw new Error('Not enough arguments')
+  if (args.length < 4) throw new Error('Not enough arguments');
 
   if (args.slice(2).every((day) => !isNaN(Number(day)))) {
-    const arr = args.slice(3).map((day) => Number(day))
-    const target = Number(args[2])
+    const arr = args.slice(3).map((day) => Number(day));
+    const target = Number(args[2]);
 
     return {
       arr,
       target,
-    }
+    };
   } else {
-    throw new Error('Provided values were not numbers!')
+    throw new Error('Provided values were not numbers!');
   }
-}
+};
 
 const calculateExercises = (
   arr: Array<number>,
   target: number
 ): ExercisesResult => {
-  const periodLength = arr.length
-  const trainingDays = arr.filter((day) => day > 0).length
-  const success = arr.every((day) => day > target)
-  const ratingDescription = String(arr.filter((e) => e >= target).length)
-  const average = arr.reduce((a, b) => a + b) / periodLength
+  const periodLength = arr.length;
+  const trainingDays = arr.filter((day) => day > 0).length;
+  const success = arr.every((day) => day > target);
+  const average = arr.reduce((a, b) => a + b) / periodLength;
 
-  const ratingDescriptionList = ['poorly', 'good', 'perfect']
+  const ratingDescriptionList = ['poorly', 'good', 'perfect'];
 
-  let rating
-  const successDays = arr.filter((day) => day > target).length
+  let rating;
+  const successDays = arr.filter((day) => day > target).length;
   if (successDays === periodLength) {
-    rating = 3
+    rating = 3;
   } else if (successDays > periodLength / 2) {
-    rating = 2
+    rating = 2;
   } else {
-    rating = 1
+    rating = 1;
   }
 
   return {
@@ -59,16 +58,18 @@ const calculateExercises = (
     ratingDescription: ratingDescriptionList[rating - 1],
     target,
     average,
-  }
-}
+  };
+};
 
 try {
-  const { arr, target } = parseArguments(process.argv)
-  console.log(calculateExercises(arr, target))
+  const { arr, target } = parseArguments(process.argv);
+  console.log(calculateExercises(arr, target));
 } catch (error: unknown) {
-  let errorMessage = 'Something bad happened.'
+  let errorMessage = 'Something bad happened.';
   if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message
+    errorMessage += ' Error: ' + error.message;
   }
-  console.log(errorMessage)
+  console.log(errorMessage);
 }
+
+export default calculateExercises;
