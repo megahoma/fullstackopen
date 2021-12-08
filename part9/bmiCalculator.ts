4bd3a29f0622bef6bcab995e97750ed7ccab0d12
megahoma <timkrut5348@gmail.com>
2021-12-08 09:17:56 +0300

    part9 (Exercises 9.1 - 9.3)

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,46 @@
+interface BmiInputType {
+  height: number
+  weight: number
+}
+
+const parseArguments = (args: Array<string>): BmiInputType => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    }
+  } else {
+    throw new Error('Provided values were not numbers!')
+  }
+}
+
+const calculateBmi = (height: number, weight: number): string => {
+  if (height <= 0 || weight <= 0) {
+    throw new Error("Can't divide by 0!")
+  }
+
+  const bmi = weight / (height / 100) ** 2
+
+  if (bmi < 18.5) {
+    return 'Underweight'
+  } else if (18.5 <= bmi && bmi <= 24.9) {
+    return 'Overweight (not healthy weight)'
+  } else if (25 <= bmi && bmi <= 29.9) {
+    return 'Normal (healthy weight)'
+  } else {
+    return 'Obesity (not healthy weight)'
+  }
+}
+
+try {
+  const { height, weight } = parseArguments(process.argv)
+  console.log(calculateBmi(height, weight))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message
+  }
+  console.log(errorMessage)
+}
---
+++ part9/trainingCalculator.ts
@@ -0,0 +1,74 @@
+interface ExercisesResult {
+  periodLength: number
+  trainingDays: number
+  success: boolean
+  rating: number
+  ratingDescription: string
+  target: number
+  average: number
+}
+
+interface ExercisesInputType {
+  arr: number[]
+  target: number
+}
+
+const parseArguments = (args: Array<string>): ExercisesInputType => {
+  if (args.length < 4) throw new Error('Not enough arguments')
+
+  if (args.slice(2).every((day) => !isNaN(Number(day)))) {
+    const arr = args.slice(3).map((day) => Number(day))
+    const target = Number(args[2])
+
+    return {
+      arr,
+      target,
+    }
+  } else {
+    throw new Error('Provided values were not numbers!')
+  }
+}
+
+const calculateExercises = (
+  arr: Array<number>,
+  target: number
+): ExercisesResult => {
+  const periodLength = arr.length
+  const trainingDays = arr.filter((day) => day > 0).length
+  const success = arr.every((day) => day > target)
+  const ratingDescription = String(arr.filter((e) => e >= target).length)
+  const average = arr.reduce((a, b) => a + b) / periodLength
+
+  const ratingDescriptionList = ['poorly', 'good', 'perfect']
+
+  let rating
+  const successDays = arr.filter((day) => day > target).length
+  if (successDays === periodLength) {
+    rating = 3
+  } else if (successDays > periodLength / 2) {
+    rating = 2
+  } else {
+    rating = 1
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription: ratingDescriptionList[rating - 1],
+    target,
+    average,
+  }
+}
+
+try {
+  const { arr, target } = parseArguments(process.argv)
+  console.log(calculateExercises(arr, target))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message
+  }
+  console.log(errorMessage)
+}
